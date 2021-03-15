1dcca5f353215a7558ac7c6a1283696d93279289
Shenglai <lyellooo@gmail.com>
2021-03-15 20:54:54 +0000

    BINF-481: fix entry

--- samtools_mpileup_tool/__main__.py
+++ samtools_mpileup_tool/__main__.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
+import sys
 
 from samtools_mpileup_tool import multi_samtools_mpileup
 
 if __name__ == "__main__":
-    multi_samtools_mpileup.main()
+    # CLI Entrypoint.
+    retcode = 0
+
+    try:
+        retcode = multi_samtools_mpileup.main()
+
+    except Exception as e:
+        retcode = 1
+
+    sys.exit(retcode)
 
 # __END__
