7ad8b4c4841907e880d50fd3ad5560aeb1f9748a
linghao <sxwcasd@gmail.com>
2024-08-05 16:08:03 +0000

    update result assign

--- samtools_mpileup_tool/multi_samtools_mpileup.py
+++ samtools_mpileup_tool/multi_samtools_mpileup.py
@@ -113,8 +113,8 @@ def tpe_submit_commands(
                 logger.info(result.stderr)
             except Exception as e:
                 exceptions.append(cmd)
-                logger.error(result.stdout)
-                logger.error(result.stderr)
+                logger.error(f"Exception occurred for command {cmd}: {e}")
+                raise e
     return exceptions
 
 
